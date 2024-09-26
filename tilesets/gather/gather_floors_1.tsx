73c9a55d236a6e29b59d93b17fa50670615a6a02
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2024-09-25 23:32:33 -0300

    feat: pinaculo map

---
+++ tilesets/gather/gather_floors_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="gather_floors_1" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
+ <image source="gather_floors_1.png" width="1600" height="1600"/>
+</tileset>
